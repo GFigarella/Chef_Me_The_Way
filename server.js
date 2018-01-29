ca9fc801eb318478c385c3fb20c1b1b9bd5278a2
Evan Darst <evan.l.darst@gmail.com>
2018-01-29 11:55:52 -0500

    testing for search and querying API

--- server.js
+++ server.js
@@ -16,9 +16,36 @@ var flash    = require('connect-flash');
 // configuration ===============================================================
 // connect to our database
 
-require('./config/passport')(passport); // pass passport for configuration
+// require('./config/passport')(passport); // pass passport for configuration
 
+//testtesttest
 
+var NutritionixClient = require('nutritionix');
+var nutritionix = new NutritionixClient({
+    appId: 'da81ce22',
+    appKey: '628619f77ba41aa72c5b3d4360601da6'
+    // debug: true, // defaults to false 
+});
+
+app.get("/api", function (req, res) {
+
+    console.log("Hello?");
+
+    nutritionix.search.standard({
+        q: 'crouton',
+        // use these for paging 
+        limit: 10,
+        offset: 0,
+        // controls the basic nutrient returned in search 
+        search_nutrient: 'calories'
+    }).then(successHandler, errorHandler)
+        .catch(uncaughtExceptionHandler);
+
+	console.log(res);
+
+});
+	
+	///gtestetewtwetew
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
@@ -39,6 +66,9 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
+
+//TEST
+
 
 
 // routes ======================================================================
