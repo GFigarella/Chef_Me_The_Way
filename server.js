499596ff3d7508790bbd2d820faa1623bf5ad92f
Alakh Sheth <alakhdeva@gmail.com>
2018-01-29 10:43:01 -0500

    made some css stuff

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var app      = express();
 var port     = process.env.PORT || 8080;
+var path = require('path');
 
 var passport = require('passport');
 var flash    = require('connect-flash');
@@ -21,6 +22,7 @@ require('./config/passport')(passport); // pass passport for configuration
 
 
 // set up our express application
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.urlencoded({
