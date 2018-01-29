d8afd9f9c1a5d8b24dbd45621f452f7bb9ec42b1
Evan Darst <evan.l.darst@gmail.com>
2018-01-29 10:45:20 -0500

    setting up API routing to pull in from food API node package

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,3 +1,15 @@
+//Require models directory
+
+var db = require("../models");
+
+//Set up for exporting pulled data from API
+
 module.exports = function(app, passport) {
+
+    app.get("/api/posts"), function(req, res) {
+
+        var query = {};
+        if (req.query)
+    }
 
 }
