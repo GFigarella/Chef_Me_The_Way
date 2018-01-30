0d8f66d2046c9efb5a8b7700f452d1f23a8422e7
Evan Darst <evan.l.darst@gmail.com>
2018-01-30 11:51:54 -0500

    getting API return from Axios get method, added in file for foodsearch using Handlebars

--- models/food.js
+++ models/food.js
@@ -1,33 +1,28 @@
+var axios = require("axios");
 var express = require('express');
 var app = express();
+var bodyParser = ("body-parser");
 
-var NutritionixClient = require('nutritionix');
-var nutritionix = new NutritionixClient({
-    appId: 'da81ce22',
-    appKey: '628619f77ba41aa72c5b3d4360601da6'
-    // debug: true, // defaults to false 
-});
+//Perform a for loop through the API for the user
 
-var foodSearch = function(req, res, next) {
+axios.get("https://api.edamam.com/search?&q=chicken&app_id=4a5d81a2&app_key=379308ab9da9a8ee47f63563d2774ac4&from=0&to=9&q=", {
+})
+.then(function (response) {
+    console.log(response.data.hits);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
-    console.log("This is working");
-};
+//   var recipeRes = response.data.hits.recipe;
 
-app.use(foodSearch);
+//   res.render
 
-//testing for API return
+  //for this
 
-app.get("/api/", function(req, res) {
+  //if response is data hits recipe
 
-    nutritionix.search.standard({
-        q:'salad',
-        // use these for paging 
-        limit: 10,
-        offset: 0,
-        // controls the basic nutrient returned in search 
-        search_nutrient: 'calories'
-      }).then(successHandler, errorHandler)
-        .catch(uncaughtExceptionHandler);
-});
+  //the
 
+// app.use(foodSearch);
 app.listen(8080);
